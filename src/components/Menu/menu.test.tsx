f15f585a4440a6333a4c91e4987b4e6f3b91eb1d
Ezreal <xinpengma217603@sohu-inc.com>
2022-03-27 22:17:30 +0800

    feat(submenu): add unit test example

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -3,9 +3,11 @@ import {
   fireEvent,
   render,
   RenderResult,
+  waitFor,
 } from "@testing-library/react";
 import Menu, { MenuProps } from "./menu";
 import MenuItem from "./menuItem";
+import SubMenu from "./subMenu";
 
 const testProps: MenuProps = {
   defaultIndex: "0",
@@ -22,8 +24,25 @@ const generateMenu = (props: MenuProps) => {
       <MenuItem>active</MenuItem>
       <MenuItem disabled>disabled</MenuItem>
       <MenuItem>xyz</MenuItem>
+      <SubMenu title="dropdown">
+        <MenuItem>dropdown 1</MenuItem>
+      </SubMenu>
     </Menu>
   );
+};
+const createStyleFile = () => {
+  const cssStr: string = `
+        .mxp-submenu {
+            display:none;
+        }
+        .mxp-submenu.menu-opened{
+            display: block;
+        }
+    `;
+  const style = document.createElement("style");
+  style.type = "text/css";
+  style.innerHTML = cssStr;
+  return style;
 };
 let wrapper: RenderResult,
   menuEl: HTMLElement,
@@ -32,6 +51,7 @@ let wrapper: RenderResult,
 describe("test Menu and MenuItem Component", () => {
   beforeEach(() => {
     wrapper = render(generateMenu(testProps));
+    wrapper.container.append(createStyleFile());
     menuEl = wrapper.getByTestId("test-menu");
     activeEl = wrapper.getByText("active");
     disableEl = wrapper.getByText("disabled");
@@ -40,7 +60,8 @@ describe("test Menu and MenuItem Component", () => {
   it("should render correct menu and menu-item with default props", () => {
     expect(menuEl).toBeInTheDocument();
     expect(menuEl).toHaveClass("mxp-menu test");
-    expect(menuEl.getElementsByTagName("li").length).toEqual(3);
+    // expect(menuEl.getElementsByTagName("li").length).toEqual(3);
+    expect(menuEl.querySelectorAll(":scope > li").length).toEqual(4);
     expect(activeEl).toHaveClass("mxp-menu-item is-active");
     expect(disableEl).toHaveClass("mxp-menu-item is-disabled");
   });
@@ -59,5 +80,26 @@ describe("test Menu and MenuItem Component", () => {
     const wrapper = render(generateMenu(testVerticalProps));
     const menuEl = wrapper.getByTestId("test-menu");
     expect(menuEl).toHaveClass("menu-vertical");
+  });
+  it("should dropdown items when hover on subMenu", async () => {
+    expect(wrapper.queryByText("dropdown 1")).not.toBeVisible();
+    const dropdownEl = wrapper.getByText("dropdown");
+    fireEvent.mouseEnter(dropdownEl);
+    await waitFor(() => {
+      expect(wrapper.queryByText("dropdown 1")).toBeVisible();
+    });
+    fireEvent.click(wrapper.getByText("dropdown 1"));
+    expect(testProps.onSelect).toHaveBeenCalledWith("3-0");
+    fireEvent.mouseLeave(dropdownEl);
+    await waitFor(() => {
+      expect(wrapper.queryByText("dropdown 1")).not.toBeVisible();
+    });
+  });
+  it("should drop items when click title in vertical mode", () => {
+    cleanup();
+    const wrapper = render(generateMenu(testVerticalProps));
+    const dropdownEl = wrapper.getByText("dropdown");
+    fireEvent.click(dropdownEl);
+    expect(wrapper.queryByText("dropdown 1")).toBeVisible();
   });
 });
