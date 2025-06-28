2d09caf13c18beb8e1405935951474a548e60ede
mxp <1538139147@qq.com>
2025-06-29 02:51:06 +0800

    fix(ci): revert fix pnpm version

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,28 +1,29 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-interactions",
-    {
-      name: "@storybook/addon-styling",
-      // options: {
-      //   sass: {
-      //     implementation: require('sass')
-      //   }
-      // }
-    }
-  ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
-  staticDirs: ["../public"]
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-interactions",
+        {
+            name: "@storybook/addon-styling",
+            // options: {
+            //   sass: {
+            //     implementation: require('sass')
+            //   }
+            // }
+        },
+    ],
+    framework: {
+        name: "@storybook/react-webpack5",
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
+    staticDirs: ["../public"],
 };
+
 export default config;
